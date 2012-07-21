d1ac384633b2bfd6d4e7d9c25ad339b727b4f161
Your Name <drew@drewco.de>
2012-07-21 00:54:48 -0400

    Added installer

---
+++ install.sh
@@ -0,0 +1,28 @@
+function die()
+{
+    echo "${@}"
+    exit 1
+}
+
+if [ -d ~/.dotfiles ]
+then
+  echo "\033[0;33mYou already have Oh My Zsh installed.\033[0m You'll need to remove ~/.oh-my-zsh if you want to install"
+  exit
+fi
+
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh;
+
+curl -Lo- https://bit.ly/janus-bootstrap | sh;
+
+echo "\033[0;34mCloning dotfiles...\033[0m"
+hash git >/dev/null && /usr/bin/env git clone --recursive https://github.com/drewcode/dotfiles.git ~/.dotfiles || {
+  echo "git not installed"
+  exit
+}
+
+if [ -d ~/.dotfiles ]
+then
+ cd $HOME/.dotfiles || die "Could not go into the ${HOME}/.dotfiles"
+ rake || die "Rake failed."
+fi
+
